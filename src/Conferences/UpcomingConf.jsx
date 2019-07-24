46c5900abcd8fa886f0233e5a53334c76b892c7d
Kirby Campbell <jkirbycampbell@gmail.com>
2019-07-23 20:40:53 -0700

    Update Bottom Main Layout

--- src/Conferences/UpcomingConf.jsx
+++ src/Conferences/UpcomingConf.jsx
@@ -5,7 +5,6 @@ const UpcomingConf = () => {
   return (
     <div>
       <div className="Upcoming-Conf">Upcoming Conferences</div>
-      {/* TODO: Make this mappable from a javascript file */}
       <div className="Double-Column-Conf">
         <img className="bullet-logo" src={BulletLogo} alt="Small-Logo" />
         <div className="Conf-Detail">Test Info</div>
