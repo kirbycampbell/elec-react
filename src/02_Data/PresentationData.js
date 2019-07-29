4db198699ffa74f21be242909833a477fb54d3a4
Kirby Campbell <jkirbycampbell@gmail.com>
2019-07-29 14:47:30 -0700

    Presentations Added!

--- src/02_Data/PresentationData.js
+++ src/02_Data/PresentationData.js
@@ -1,16 +1,154 @@
 export const Presentations2017 = [
   {
     name:
-      "Kevin Parrett & Sarah Greenfield - Oregon DEQs Perspectives on Portland Harbor (2 mb)",
-    pdf: "2017-05-19-parrettgreenfield.pdf"
+      'Kevin Parrett & Sarah Greenfield - Oregon DEQs Perspectives on Portland Harbor (2 mb)',
+    pdf: '2017-05-19-parrettgreenfield.pdf',
   },
   {
     name:
-      "Mary Lou Soscia - Willamette River Toxics Reduction Partnership (1.3 mb)",
-    pdf: "2017-05-19-soscia.pdf"
+      'Mary Lou Soscia - Willamette River Toxics Reduction Partnership (1.3 mb)',
+    pdf: '2017-05-19-soscia.pdf',
   },
   {
-    name: "Keith Johnson - Controlling Sources to Portland Harbor (1.5 mb)",
-    pdf: "2017-05-19-Johnson.pdf"
-  }
+    name: 'Keith Johnson - Controlling Sources to Portland Harbor (1.5 mb)',
+    pdf: '2017-05-19-Johnson.pdf',
+  },
+  {
+    name: 'Joan Snyder - Portland Harbor in the Post-ROD World (1.7 mb)',
+    pdf: '2017-05-19-snyder.pdf',
+  },
+  {
+    name:
+      'Chris Moody - CERCLA & Clean Water Act Coordination Strategies (1.2 mb)',
+    pdf: '2017-05-19-moody.pdf',
+  },
+  {
+    name:
+      'Alex Liverman - Preventing Sediment Recontamination by Leveraging... (1.9 mb)',
+    pdf: '2017-05-19-liverman.pdf',
+  },
+  {
+    name:
+      'Brad Hermanson - Managing Programs for Cross-Program Efficiencies (1.9 mb)',
+    pdf: '2017-05-19-hermanson.pdf',
+  },
+  {
+    name:
+      'Matt McClincy - Portland Harbor Post ROD Source Control Status (1.8 mb)',
+    pdf: '2017-05-19-mcclincy.pdf',
+  },
+  {
+    name: 'Erin McDonnell - Case Study: Premier Edible Oils (2.5 mb)',
+    pdf: '2017-05-19-mcdonnell.pdf',
+  },
+  {
+    name: 'Brendan Robinson - PEO Groundwater SCM (1.7 mb)',
+    pdf: '2017-05-19-Robinson.pdf',
+  },
+  {
+    name: 'Jim McKenna - State of Oregon Perspectives: Superfund Site (1.8 mb)',
+    pdf: '2017-05-19-mckenna.pdf',
+  },
+];
+
+export const Presentations2016 = [
+  {
+    name: 'Leah Feldon - Air Quality Programs in Oregon',
+    pdf: '2016-04-18-feldon.pdf',
+  },
+  {
+    name: 'Thomas Wood - Legal Considerations: State and Federal',
+    pdf: '2016-04-18-wood.pdf',
+  },
+  {
+    name: 'Merlyn Hough - Air Quality Protection in Lane County (3.5 mb)',
+    pdf: '2016-04-18-hough.pdf',
+  },
+  {
+    name: 'David Farrer - Air Quality, Science and Public Health',
+    pdf: '2016-04-18-farrer.pdf',
+  },
+  {
+    name: 'John Browning - Air Toxics Technical Considerations (1.2 mb)',
+    pdf: '2016-04-18-browning.pdf',
+  },
+];
+
+export const Presentations2015 = [
+  {
+    name: 'Kris McCaig - Upper Columbia River RI/FS (1.4 mb)',
+    pdf: '2015-06-08-McCaig.pdf',
+  },
+  {
+    name:
+      'Alison Hiltner and Elly Hale - Lower Duwamish Waterway Record of Decision (3.5 mb)',
+    pdf: '2015-06-08-HiltnerHale.pdf',
+  },
+  {
+    name: 'Kurt Peterson - Allocating the Costs of Sediment Cleanups',
+    pdf: '2015-06-08-Peterson.pdf',
+  },
+  {
+    name: 'Reid Carscadden - The Real Cost of Sediment Remediation',
+    pdf: '2015-06-08-Carscadden.pdf',
+  },
+  {
+    name: 'Jim Pendowski - Priorities for Environmental Cleanup (1.1 mb)',
+    pdf: '2015-06-08-Pendowski.pdf',
+  },
+  {
+    name: 'Ivy Anderson - Advanced Sediments, Coordinating Cleanup (1.3 mb)',
+    pdf: '2015-06-08-Anderson.pdf',
+  },
+  {
+    name:
+      'Mike Stoner- The Port of Bellingham, Revitalizing Bellinghams Waterfront(2.3 mb)',
+    pdf: '2015-06-08-Stoner.pdf',
+  },
+  {
+    name:
+      'Barry Kellems - Considering Climate Change Impacts in Selection and Design of Sediment Remedies',
+    pdf: '2015-06-08-Kellems.pdf',
+  },
+  {
+    name:
+      'Beth Sheldrake - Integrating Climate Change Adaptation into the Superfund Program (1.4 mb)',
+    pdf: '2015-06-08-Sheldrake.pdf',
+  },
+];
+export const Presentations2014 = [
+  {
+    name: 'Heather Bartlett - Protecting Water Quality in Washington',
+    pdf: '2014-11-17-bartlett.pdf',
+  },
+  {
+    name:
+      'Angela Chung - Latest Developments in Human Health Water Quality Criteria. An EPA Perspective',
+    pdf: '2014-11-17-chung.pdf',
+  },
+  {
+    name:
+      'Nancy Judd - Impacts of Washingtons Proposals on Businesses and NPDES Permits',
+    pdf: '2014-11-17-judd.pdf',
+  },
+  {
+    name:
+      'Douglas Steding - Legal Impacts of Proposed Water Quality Standards and Possible Litigation Scenarios',
+    pdf: '2014-11-17-steding.pdf',
+  },
+  {
+    name:
+      'Cheryl Niemi - Washington Proposed Rules: Preliminary Draft Rule Package, Water Quality Standards for Human Health Criteria, and Implementation Tools',
+    pdf: '2014-11-17-niemi.pdf',
+  },
+  {
+    name:
+      'Tracy Collier - Source Control, Toxic Chemicals, and Biological Improvements (1.2 mb)',
+    pdf: '2014-11-17-collier.pdf',
+  },
+  {
+    name:
+      'Kris Hendrickson - Impacts of Proposed Water Quality Standards on Environmental Cleanup Projects',
+    pdf: '2014-11-17-hendrickson.pdf',
+  },
 ];
--- src/Presentations/Presentations.jsx
+++ src/Presentations/Presentations.jsx
@@ -1,35 +1,98 @@
-import React from "react";
-import UpcomingConf from "../Conferences/UpcomingConf";
-import SmallMiddleSection from "../01_Static_Components/SmallMiddleSection/SmallMiddleSection";
-import { Presentations2017 } from "../02_Data/PresentationData";
-// import "../02_Data/pdf-presentations";
+import React from 'react';
+import UpcomingConf from '../Conferences/UpcomingConf';
+import SmallMiddleSection from '../01_Static_Components/SmallMiddleSection/SmallMiddleSection';
+import {
+  Presentations2017,
+  Presentations2016,
+  Presentations2015,
+  Presentations2014,
+} from '../02_Data/PresentationData';
+import './Presentations.css';
 
 const Presentations = () => {
   return (
     <React.Fragment>
       <SmallMiddleSection />
-      <div className="Main-Section" style={{ minHeight: "34vh" }}>
+      <div className="Main-Section" style={{minHeight: '34vh'}}>
         <div className="Outer-Body">
           <div className="UpcomingConf">
             <UpcomingConf />
           </div>
-          <div className="Body-Right">
-            Presentations
+          <div className="Pres-Body-Right">
             <div>
-              {Presentations2017.map(pres => {
-                return (
-                  <div key={pres.name}>
-                    <a
-                      href={
-                        process.env.PUBLIC_URL +
-                        `/pdf-presentations/${pres.pdf}`
-                      }
-                    >
-                      {pres.name}
-                    </a>
-                  </div>
-                );
-              })}
+              <h1>Presentations</h1>
+              <div className="Ind-Pres">
+                <h3>2017 Oregon Source Control Conference, in Portland</h3>
+                {Presentations2017.map(pres => {
+                  return (
+                    <li key={pres.name} className="pres-sec">
+                      <a
+                        href={
+                          process.env.PUBLIC_URL +
+                          `/pdf-presentations/${pres.pdf}`
+                        }
+                        className="pres-link"
+                      >
+                        {pres.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </div>
+              <div className="Ind-Pres">
+                <h3>2016 Advanced Air Quality Seminar, in Portland</h3>
+                {Presentations2016.map(pres => {
+                  return (
+                    <li key={pres.name} className="pres-sec">
+                      <a
+                        href={
+                          process.env.PUBLIC_URL +
+                          `/pdf-presentations/${pres.pdf}`
+                        }
+                        className="pres-link"
+                      >
+                        {pres.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </div>
+              <div className="Ind-Pres">
+                <h3>2015 Environmental Cleanup Conference, in Seattle</h3>
+                {Presentations2015.map(pres => {
+                  return (
+                    <li key={pres.name} className="pres-sec">
+                      <a
+                        href={
+                          process.env.PUBLIC_URL +
+                          `/pdf-presentations/${pres.pdf}`
+                        }
+                        className="pres-link"
+                      >
+                        {pres.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </div>
+              <div className="Ind-Pres">
+                <h3>2014 Toxics Reduction Conference</h3>
+                {Presentations2014.map(pres => {
+                  return (
+                    <li key={pres.name} className="pres-sec">
+                      <a
+                        href={
+                          process.env.PUBLIC_URL +
+                          `/pdf-presentations/${pres.pdf}`
+                        }
+                        className="pres-link"
+                      >
+                        {pres.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
