508acb99599802e2086518a330158b98395e45e1
Kirby Campbell <jkirbycampbell@gmail.com>
2019-08-03 18:40:55 -0700

    Updated Register Page

--- src/Conferences/ConfPage.jsx
+++ src/Conferences/ConfPage.jsx
@@ -1,44 +1,54 @@
-import React from "react";
-import "./Conferences.css";
-import "./ConfPage.css";
-import UpcomingConf from "./UpcomingConf";
-import SmallMiddleSection from "../01_Static_Components/SmallMiddleSection/SmallMiddleSection";
-import { conferenceList } from "../02_Data/ConferenceData";
-import SmallRegistrationBox from "./Register/SmallRegistrationBox";
+import React from 'react';
+import './Conferences.css';
+import './ConfPage.css';
+import SmallMiddleSection
+  from '../01_Static_Components/SmallMiddleSection/SmallMiddleSection';
+import {conferenceList} from '../02_Data/ConferenceData';
+import SmallRegistrationBox from './Register/SmallRegistrationBox';
 
-const ConfPage = ({ match }) => {
+const ConfPage = ({match}) => {
   let conf = {};
 
   for (var key in conferenceList) {
-    if (conferenceList.hasOwnProperty(key)) {
-      if (conferenceList[key].id === parseInt(match.params.id, 10)) {
+    if (conferenceList.hasOwnProperty (key)) {
+      if (conferenceList[key].id === parseInt (match.params.id, 10)) {
         conf = conferenceList[key]; // Assigns conf to upcoming conference
       }
     }
   }
+  console.log (conf);
 
   return (
     <React.Fragment>
       <SmallMiddleSection />
-      <div className="Main-Section" style={{ minHeight: "34vh" }}>
+      <div className="Main-Section" style={{minHeight: '34vh'}}>
         <div className="Outer-Body">
           <div className="UpcomingConf">
-            <SmallRegistrationBox />
+            <SmallRegistrationBox reg={conf.registration} />
           </div>
           <div className="Body-Right">
             <div className="Conf-Page-Title">{conf.title}</div>
             <div className="Conf-Page-Org">{conf.org}</div>
             <div className="Conf-Page-Extra">{conf.extra}</div>
             <div>
-              {conf.schedule.map(c => {
+              {conf.schedule.map (c => {
                 return (
                   <div className="Time-Section" key={c[0]}>
                     <div className="Conf-Split">
                       <div className="Time">{c[0]}</div>
                       <div className="Flex-Col">
-                        {c.map(function(con, index) {
-                          if (index > 0) {
-                            return <div key={con}>{con}</div>;
+                        {c.map (function (con, index) {
+                          if (index > 0 && index % 2 === 1 && con.length > 0) {
+                            return (
+                              <div className="odd-style" key={con}>{con}</div>
+                            );
+                          }
+                          if (index > 0 && index % 2 === 0 && con.length > 0) {
+                            return (
+                              <div className="even-style" key={con}>
+                                <li>{con}</li>
+                              </div>
+                            );
                           }
                         })}
                       </div>
--- src/Conferences/Register/SmallRegistrationBox.jsx
+++ src/Conferences/Register/SmallRegistrationBox.jsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React from 'react';
+import './SmallBox.css';
+import {Link} from 'react-router-dom';
 
 const SmallRegistrationBox = props => {
   return (
-    <div>
-      <div>Registration</div>
-      <div>Section</div>
+    <div className="Outer-Small-Box">
+      {props.reg &&
+        <div>
+          <div className="Title-Small-Box">Registration</div>
+          <div className="Inner-Small-Box">
+            <div className="Register-Section">
+              <div className="Small-Text-Box">
+                Secure online registration with credit card:
+              </div>
+              <button className="Small-Box-BTN">
+                <Link to="/Register/">Register online</Link>
+              </button>
+            </div>
+            <div className="Register-Section">
+              <div className="Small-Text-Box">
+                Register online w/o credit card (mail check):
+              </div>
+              <button className="Small-Box-BTN">Register</button>
+            </div>
+            <div className="Register-Section">
+              <div className="Small-Text-Box">Registration by fax or mail:</div>
+              <button className="Small-Box-BTN">
+                <a href={process.env.PUBLIC_URL + `/agenda-pdfs/${props.reg}`}>
+                  Printable Form
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>}
+      {props.agenda &&
+        <div>
+          <div className="Title-Small-Box">Conference Agenda</div>
+          <div className="Inner-Small-Box">
+
+            <div className="Register-Section">
+              <div className="Small-Text-Box">Agenda Printout</div>
+              <button className="Small-Box-BTN">
+                <a
+                  href={process.env.PUBLIC_URL + `/pdf-agendas/${props.agenda}`}
+                >
+                  Printable Friendly Agenda
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>}
     </div>
   );
 };
